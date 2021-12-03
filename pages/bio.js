3149cd55b5addb0f9ae350b442cb34e7f45f10c9
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-12-03 11:57:07 -0700

    my edit

---
+++ pages/bio.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Meta from "../components/Meta";
+
+export default function bio() {
+  return (
+    <div>
+      <Meta title="Bio" />
+      <h1>My name is Dylan</h1>
+    </div>
+  );
+}
